9701fabb2db64a21a2344ca9cf719ec4eedb78b4
Pete <peterwbrodie@gmail.com>
2021-03-03 11:34:37 +0000

    added test for drink_has_name

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,8 @@
 import unittest
-from tests.customer_tests import TestCustomer
+# from tests.customer_tests import TestCustomer
 from tests.drink_tests import TestDrink
-from tests.pub_tests import TestPub
+# from tests.pub_tests import TestPub
 
 if __name__ == "__main__":
     unittest.main()
-    
+
--- tests/drink_tests.py
+++ tests/drink_tests.py
@@ -1,7 +1,12 @@
 import unittest
-from src.customer import Customer
+# from src.customer import Customer
 from src.drink import Drink
-from src.pub import Pub
+# from src.pub import Pub
 
 class TestDrink (unittest.TestCase):
-    pass
+    def setUp(self):
+        self.drink_1 = Drink("Mojito", 8.0)
+        self.drink_2 = Drink("Pilsen", 6.5)
+
+    def test_drink_has_name(self):
+        self.assertEqual("Mojito", self.drink_1.name)
