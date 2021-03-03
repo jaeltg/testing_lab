92279fd1ee504ce9871ecabacfee26003eb49df6
Pete <peterwbrodie@gmail.com>
2021-03-03 13:28:16 +0000

    test customer has name

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,5 @@
 import unittest
-# from tests.customer_tests import TestCustomer
+from tests.customer_tests import TestCustomer
 from tests.drink_tests import TestDrink
 from tests.pub_tests import TestPub
 
--- tests/customer_tests.py
+++ tests/customer_tests.py
@@ -4,4 +4,9 @@ from src.drink import Drink
 from src.pub import Pub
 
 class TestCustomer (unittest.TestCase):
-    pass
+    def setUp(self):
+        self.customer = Customer("Malcolm", 25.0)
+        self.drink = Drink("Pilsen", 6.5)
+
+    def test_customer_has_name(self):
+        self.assertEqual("Malcolm", self.customer.name)   
